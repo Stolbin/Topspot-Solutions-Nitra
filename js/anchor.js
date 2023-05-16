5217f70f7f16e0ab5375ff8cbb1b405464034daa
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-05-16 16:01:28 +0300

    upd

--- js/anchor.js
+++ js/anchor.js
@@ -1,52 +1,52 @@
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#home"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
   return false;
 });
 
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#about"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
   return false;
 });
 
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#product"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
   return false;
 });
 
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#markets"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
   return false;
 });
 
-var $page = $('html, body');
+var $page = $("html, body");
 $('a[href*="#footer"]').click(function () {
   $page.animate(
     {
-      scrollTop: $($.attr(this, 'href')).offset().top,
+      scrollTop: $($.attr(this, "href")).offset().top,
     },
     300
   );
---
+++ js/form.js
@@ -0,0 +1,38 @@
+const TOKEN = "6295958096:AAEpfHzcgg2SVWGo2JBxrJWnJYimDvgP9hY";
+const CHAT_ID = "-1001848182059";
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+const seccess = document.getElementById("succese");
+
+document.getElementById("form").addEventListener("submit", handkerSubmit);
+
+function handkerSubmit(e) {
+  e.preventDefault();
+
+  let message = `<b>Заявка с сайта</b>\n`;
+  message += `<b>User: </b> ${this.name.value}\n`;
+  message += `<b>Email: </b> ${this.email.value}\n`;
+  message += `<b>Phone: </b> ${this.phone.value}\n`;
+  message += `<b>Subject: </b> ${this.subject.value}\n`;
+  message += `<b>Somments: </b> ${this.comments.value}\n`;
+
+  axios
+    .post(URL_API, {
+      chat_id: CHAT_ID,
+      parse_mode: "html",
+      text: message,
+    })
+    .then((res) => {
+      this.name.value = "";
+      this.email.value = "";
+      this.phone.value = "";
+      this.subject.value = "";
+      this.comments.value = "";
+      seccess.style.display = "block";
+    })
+    .catch((err) => {
+      console.wrm("err");
+    })
+    .finally(() => {
+      console.log("End");
+    });
+}
