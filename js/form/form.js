458655ec1f0a695f4830a6d6def4bc0087dc1567
Stolbin <109080579+Stolbin@users.noreply.github.com>
2023-06-27 14:24:01 +0300

    upd

--- js/form/form.js
+++ js/form/form.js
@@ -2,15 +2,10 @@ const TOKEN = "6218237968:AAF-aFWB12uinILXIsZfi9kMi1Sdr_CJvs8";
 const CHAT_ID = "-1001751122058";
 const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 const succese = document.getElementById("succese");
+const succeseContent = document.getElementById("succese_content");
 const formSubmit = document.getElementById("form");
-const inputs = document.getElementsByClassName("inputs-list-form");
-const textarea = document.getElementsByClassName("textarea-item-form");
-const btnSubmit = document.getElementsByClassName("btn-footer-form");
 const form = document.getElementById("form");
 form.addEventListener("submit", handkerSubmit);
-
-// const formElemts = [inputs, textarea, btnSubmit].join("");
-console.log(inputs);
 
 function handkerSubmit(e) {
   e.preventDefault();
@@ -20,7 +15,7 @@ function handkerSubmit(e) {
   message += `<b>Email: </b> ${this.email.value}\n`;
   message += `<b>Phone: </b> ${this.tel.value}\n`;
   message += `<b>Subject: </b> ${this.subject.value}\n`;
-  message += `<b>Somments: </b> ${this.comments.value}\n`;
+  message += `<b>Сomments: </b> ${this.comments.value}\n`;
 
   axios.post(URL_API, {
     chat_id: CHAT_ID,
@@ -28,18 +23,19 @@ function handkerSubmit(e) {
     text: message,
   });
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  delay(1000)
+  delay(1000).then((res) => {
+    this.name.value = "";
+    this.email.value = "";
+    this.tel.value = "";
+    this.subject.value = "";
+    this.comments.value = "";
+    formSubmit.classList.add("footer-form_submit");
+    succese.style.display = "block";
+  });
+  const delay2 = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  delay2(1800)
     .then((res) => {
-      this.name.value = "";
-      this.email.value = "";
-      this.tel.value = "";
-      this.subject.value = "";
-      this.comments.value = "";
-      formSubmit.classList.add("footer-form_submit");
-      inputs.style.opacity = "0";
-      textarea.style.opacity = "0";
-      btnSubmit.style.opacity = "0";
-      succese.style.display = "block";
+      succeseContent.style.display = "block";
     })
     .catch(() => {
       console.log("Error");
